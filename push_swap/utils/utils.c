236e77c0dbe251e009dafcd72b1c72e30894c99f
Zenny <havny.1017@gmail.com>
2023-05-18 11:49:54 +0000

    02

---
+++ push_swap/utils/utils.c
@@ -0,0 +1,62 @@
+#include "push_swap.h"
+
+int check_sorted(t_list **stack_a)
+{
+	t_list	*head;
+
+	head = *stack_a;
+	while (head)
+	{
+		if (head->value > head->next->value)
+			return (1);
+		head = head->next;
+	}
+    return (0);
+}
+
+
+void	ft_error(void)
+{
+	write (2, "Error\n", 6);
+	exit(1);
+}
+
+int	ft_min(t_list **stack_a)
+{
+	t_list	*head;
+	t_list	*min;
+	int		i;
+
+	head = *stack_a;
+	min = NULL;
+	i = min->value;
+	while (head)
+	{
+		if (i == NULL || head->value < i)
+		{
+			i = head;
+		}
+		head = head->next;
+	}
+	return (i);
+}
+
+int	ft_max(t_list **stack_a)
+{
+	t_list	*head;
+	t_list	*max;
+	int		i;
+
+	head = *stack_a;
+	max = NULL;
+	i = max->value;
+	while (head)
+	{
+		if (i == NULL || head->value > i)
+		{
+			i = head;
+		}
+		head = head->next;
+	}
+	return (i);
+}
